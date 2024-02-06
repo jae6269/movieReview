97322fa012d64cda5329c8791168550ebeb5a219
jae6269 <jae6269@gmail.com>
2024-02-06 23:43:58 +0900

    feat : 실습서버이용 fetch함수로 데이터 받아오기 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import ReviewList from './ReviewList';
-import mockItems from '../mock.json';
 import { useState } from 'react';
+import { getReviews } from '../api';
 function App() {
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState('rating');
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -13,6 +13,12 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
+
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <div>
@@ -20,6 +26,7 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelelte}></ReviewList>
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
