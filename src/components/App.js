e5b3bcc1d40d9f25c5f5811e0b42b866b261b3ee
jae6269 <jae6269@gmail.com>
2024-02-07 14:24:26 +0900

    feat : 페이지네이션 offset 방식 이용 더보기 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import ReviewList from './ReviewList';
 import { useEffect, useState } from 'react';
 import { getReviews } from '../api';
+const LIMIT = 6;
+
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState('createdAt');
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder('createdAt');
@@ -14,13 +18,23 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async (orderQuery) => {
-    const { reviews } = await getReviews(orderQuery);
-    setItems(reviews);
+  const handleLoadClick = async (options) => {
+    const { reviews, paging } = await getReviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoadClick({ order, offset, limit: LIMIT });
   };
 
   useEffect(() => {
-    handleLoadClick(order);
+    handleLoadClick({ order, offset: 0, LIMIT });
   }, [order]);
 
   return (
@@ -30,6 +44,9 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelelte}></ReviewList>
+      <button disabled={!hasNext} onClick={handleLoadMore}>
+        더보기
+      </button>
     </div>
   );
 }
