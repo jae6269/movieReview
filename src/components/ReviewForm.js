ee727fe7577fcc17a5c7ea674bea50b8f6aafa13
jae6269 <jae6269@gmail.com>
2024-02-08 14:35:05 +0900

    refactor : 하나의 state로 form 관리하도록 변경

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,28 +1,36 @@
 import { useState } from 'react';
 
 function ReviewForm() {
-  const [title, setTitle] = useState('');
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState('');
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const [values, setValues] = useState({
+    title: '',
+    rating: 0,
+    content: '',
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((preValues) => ({
+      ...preValues,
+      [name]: value,
+    }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ title, rating, content });
+    console.log(values);
   };
   return (
-    <form>
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange}></textarea>
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        type="number"
+        name="rating"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleChange}
+      ></textarea>
       <button type="submit">확인</button>
     </form>
   );
